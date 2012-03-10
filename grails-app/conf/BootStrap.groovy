a5e9c7147ec2ee12eb40d6e7b4418183dc6239a4
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-03-10 22:09:58 +0100

    Added test data generation in dev. mode to BootStrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,19 @@
+import grails.util.GrailsUtil;
+import blogito.Entry
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+
+	def init = { servletContext ->
+		switch(GrailsUtil.environment){
+			case "development":
+				new Entry(title:"Grails 1.1 beta is out", 				summary:"Check out the new features").save()
+				new Entry(title:"Just Released - Groovy 1.6 beta 2", 	summary:"It is looking good.").save()
+				break
+
+			case "production":
+				break
+		}
+	}
+	def destroy = {
+	}
 }
